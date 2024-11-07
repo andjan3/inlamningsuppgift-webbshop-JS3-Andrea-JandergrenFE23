00e95a09d4ee2984a79972cdc41539c8e74e90f5
andjan3 <150336624+andjan3@users.noreply.github.com>
2024-11-07 13:03:25 +0100

    Redux Slice setup

--- inlamning3-webbshop/src/index.tsx
+++ inlamning3-webbshop/src/index.tsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Webbshop from "./Webbshop";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Webbshop />
+    <Provider store={store}>
+      <Webbshop />
+    </Provider>
   </React.StrictMode>
 );
